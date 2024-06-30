26f16ebaec97c767b1a5ae820f8756c85cd779df
tripathipriyanshu20 <2k.priyanshutripathi@gmail.com>
2024-07-01 02:54:47 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
 import AddRoom from "./components/room/AddRoom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import ExistingRooms from "./components/room/ExistingRooms";
+import Home from "./components/home/home";
+import EditRoom from "./components/room/EditRoom"
+
+
 
 function App() {
   return (
     <>
-      <AddRoom />
+      <main>
+       <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+          </Routes>
+        </Router>
+      </main>
     </>
   );
 }
