c5f9349100547bc2d72c3df3434fff0323abe1e5
tripathipriyanshu20 <2k.priyanshutripathi@gmail.com>
2024-07-01 23:22:38 +0530

    commit

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -58,3 +58,25 @@ export async function deleteRoom(roomId){
     
     }
 }
+
+/* This function update a room */
+export async function updateRoom(roomId, roomData){
+    const formData = new FormData()
+    formData.append("roomType", roomData.roomType)
+    formData.append("roomPrice", roomData.roomPrice)
+    formData.append("Photo", roomData.Photo)
+    const response = await api.put(`/rooms/update/${roomId}`)
+    return response
+
+}
+
+/* This function gets a room by the id */
+export async function getRoomById(roomId){
+    try{
+        const result = await api.get(`/rooms/room/${roomId}`)
+        return result.data
+    }catch(error){
+        throw new Error(`Error fetching room ${error.message}`)
+
+    }
+}
