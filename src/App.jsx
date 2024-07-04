d526d6ab963764c5055b53d566e632cd4cf300e9
tripathipriyanshu20 <2k.priyanshutripathi@gmail.com>
2024-07-04 19:09:12 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
 import AddRoom from "./components/room/AddRoom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import ExistingRooms from "./components/room/ExistingRooms";
-import Home from "./components/home/home";
-import EditRoom from "./components/room/EditRoom"
-
-
+import Home from "./components/home/Home";
+import EditRoom from "./components/room/EditRoom";
 
 function App() {
   return (
-    <>
+    <Router>
       <main>
-       <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/edit-room/:roomId" element={<EditRoom />} />
-            <Route path="/existing-rooms" element={<ExistingRooms />} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit-room/:roomId" element={<EditRoom />} />
+          <Route path="/existing-rooms" element={<ExistingRooms />} />
+        </Routes>
       </main>
-    </>
+    </Router>
   );
 }
 
-const AppWrapper = () => {
-  return (
-    <Router>
-      <App />
-    </Router>
-  );
-};
-
-export default AppWrapper;
+export default App;
